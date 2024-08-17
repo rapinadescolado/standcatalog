e84fee9371e826fa74df31180b5d6604ec59428f
Rapina <163008675+rapinadescolado@users.noreply.github.com>
2024-08-17 17:00:38 -0300

    Atualizar o function.js

--- function.js
+++ function.js
@@ -1,12 +1,3 @@
-const params = new URLSearchParams(window.location.search);
-
-var json = {
-'omaga': 'kkkkk',
-'bruh': ['bruh1','bruh2']
-}
-
-var numero = params.get('teste');
-
-if(numero==1){
-   json.omaga = 10
+{
+'omaga': 'kkkkkkk'
 }
