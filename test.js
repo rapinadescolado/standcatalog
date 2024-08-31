178e7c5a97ca89973930ae157bf9b8af86ce0259
Rapina <163008675+rapinadescolado@users.noreply.github.com>
2024-08-30 22:33:10 -0300

    Atualizar o test.js

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
-{"cu":"1"}
+import requests
+
+dados = {"nome": "Alice", "idade": 25}
+response = requests.post("https://rapinadescolado.github.io/standcatalog/get", json=dados)
+print(response.status_code)
+print(response.json())
